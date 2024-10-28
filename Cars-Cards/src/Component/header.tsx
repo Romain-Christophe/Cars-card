91c95c5d0f66fa7476380e6aa6363930a4ec910c
FIRST_NAME LAST_NAME <you@example.com>
2024-10-28 16:04:02 +0100

    navbarok

--- Cars-Cards/src/Component/header.tsx
+++ Cars-Cards/src/Component/header.tsx
@@ -1,3 +1,29 @@
-function{
-  
+const menus = [
+  { home: "Home", id: "1" },
+  { home: "Gérer mes réservations", id: "2" },
+  { home: "Véhicules", id: "3" },
+  { home: "connextion", id: "4" },
+];
+function Header() {
+  const listItem = menus.map((produit) => {
+    return <li key={produit.id}>{produit.home}</li>;
+  });
+  return (
+    <>
+      <ul className="meunpran">
+        {" "}
+        <img
+          className="logo"
+          src="https://img.freepik.com/vecteurs-libre/logo-concessionnaire-automobile-design-plat_23-2149338507.jpg?t=st=1730125084~exp=1730128684~hmac=90925dcde1baae71a3a39caa272379100c31cabc489801ddec4b3aa8b66b5b8c&w=740"
+          alt=""
+        />{" "}
+        {listItem}
+      </ul>
+      <section className="inputB">
+        <input></input>
+        <button>Search</button>
+      </section>
+    </>
+  );
 }
+export default Header;
