d167d7f5b4753329aef39cf95e49a0dfa3eb15ed
FAlex357 <alexandrefleurantxbx@gmail.com>
2024-10-28 16:02:48 +0100

    Data_Base_OK

---
+++ Cars-Cards/src/Component/Data.tsx
@@ -0,0 +1,19 @@
+ const data = [
+  { id: 1, model: "Model S", brand: "Tesla", year: 2014, imgSrc: "https://www.google.com/search?q=2014+Tesla+Model+S&tbm=isch" },
+  { id: 2, model: "Mustang", brand: "Ford", year: 2020, imgSrc: "https://www.google.com/search?q=2020+Ford+Mustang&tbm=isch" },
+  { id: 3, model: "Corolla", brand: "Toyota", year: 2018, imgSrc: "https://www.google.com/search?q=2018+Toyota+Corolla&tbm=isch" },
+  { id: 4, model: "Camaro", brand: "Chevrolet", year: 2019, imgSrc: "https://www.google.com/search?q=2019+Chevrolet+Camaro&tbm=isch" },
+  { id: 5, model: "3 Series", brand: "BMW", year: 2016, imgSrc: "https://www.google.com/search?q=2016+BMW+3+Series&tbm=isch" },
+  { id: 6, model: "Civic", brand: "Honda", year: 2021, imgSrc: "https://www.google.com/search?q=2021+Honda+Civic&tbm=isch" },
+  { id: 7, model: "E-Class", brand: "Mercedes-Benz", year: 2017, imgSrc: "https://www.google.com/search?q=2017+Mercedes-Benz+E-Class&tbm=isch" },
+  { id: 8, model: "A4", brand: "Audi", year: 2019, imgSrc: "https://www.google.com/search?q=2019+Audi+A4&tbm=isch" },
+  { id: 9, model: "Golf", brand: "Volkswagen", year: 2015, imgSrc: "https://www.google.com/search?q=2015+Volkswagen+Golf&tbm=isch" },
+  { id: 10, model: "Altima", brand: "Nissan", year: 2020, imgSrc: "https://www.google.com/search?q=2020+Nissan+Altima&tbm=isch" },
+  { id: 11, model: "Optima", brand: "Kia", year: 2017, imgSrc: "https://www.google.com/search?q=2017+Kia+Optima&tbm=isch" },
+  { id: 12, model: "Sonata", brand: "Hyundai", year: 2018, imgSrc: "https://www.google.com/search?q=2018+Hyundai+Sonata&tbm=isch" },
+  { id: 13, model: "CX-5", brand: "Mazda", year: 2019, imgSrc: "https://www.google.com/search?q=2019+Mazda+CX-5&tbm=isch" },
+  { id: 14, model: "Wrangler", brand: "Jeep", year: 2021, imgSrc: "https://www.google.com/search?q=2021+Jeep+Wrangler&tbm=isch" },
+  { id: 15, model: "XC90", brand: "Volvo", year: 2020, imgSrc: "https://www.google.com/search?q=2020+Volvo+XC90&tbm=isch" },
+];
+
+export default data;
